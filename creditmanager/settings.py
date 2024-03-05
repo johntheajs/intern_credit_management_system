473380a941e3d8739b60c2e62044ee057b815289
johntheajs <johnthomasajs@gmail.com>
2024-03-05 23:45:37 +0530

    urls-updated-1

--- creditmanager/settings.py
+++ creditmanager/settings.py
@@ -26,11 +26,9 @@ SECRET_KEY = 'django-insecure-pzu=g6m1ew-(lr(z7yrvses$mlzlpfc^+(v&ni6#t!1#hx_5zw
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    'intern-manager-86sw.onrender.com',
     'intern-credit-management-system.onrender.com'
 ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://intern-manager-86sw.onrender.com',
     'https://intern-credit-management-system.onrender.com',    
     # Add other trusted origins as needed
 ]
